3cb7d62349efa12e68b4ef9b517eb09ba88bcf04
SatviK1608 <satvik1608@gmail.com>
2023-05-09 20:52:35 +0530

    First Deploy

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  headers: {
+    'X-RapidAPI-Key': '6362c80870msh29b9f0559ce4f01p1e4cf7jsn1519af602e8c',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
